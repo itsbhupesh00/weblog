3c892fb2747e1a89dfcb2695485d1314f73489f1
Bhupesh <mr.bhupesh2024@gmail.com>
2026-08-18 01:25:31 +0545

    final website is ready. commiting first time !!

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,16 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.slug}/`,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
+			link: `/posts/${post.data.slug}/`,
 		})),
 	});
 }
